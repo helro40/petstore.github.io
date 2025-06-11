9204357ff8e19139132bc1b604801621ec42cb4f
helro40 <u6540159@au.edu>
2025-06-11 14:06:49 +0700

    Add files via upload

---
+++ loadpets.js
@@ -0,0 +1,31 @@
+const pets = [
+    {
+        "name": "Buddy",
+        "type": "Dog",
+        "age": 3,
+        "img": "dog.jpg"
+    },
+    {
+        "name": "Trio",
+        "type": "Dog",
+        "age": 3,
+        "img": "dog03.jpg"
+    },
+    { "name": "Whiskers", "type": "Cat", "age": 2, "img": "cat.jpg" },
+    { "name": "Mittens", "type": "Cat", "age": 2, "img": "cat02.jpg" },
+
+]
+
+for(let i = 0; i < pets.length; i++) {
+    const pet = pets[i];
+    const petDiv = document.getElementById('pet-list')
+                    .appendChild(document.createElement('div'));
+    petDiv.className = 'pet';
+    petDiv.innerHTML = `
+        <img src="${pet.img}" alt="${pet.name}">
+        <h3>${pet.name}</h3>
+        <p>Type: ${pet.type}</p>
+        <p>Age: ${pet.age} years</p>
+    `;
+    // document.getElementById('pet-list').appendChild(petDiv);
+}
